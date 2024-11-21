6121c5de5452bad85cd746591e9f39e68c5d9b23
Taekyon <106549022+Taekyon@users.noreply.github.com>
2024-11-21 15:42:32 +0100

    DebutExe
    Résolution du première exercice 'DebutExe.pdf'

---
+++ Python/salut.py
@@ -0,0 +1,7 @@
+username = input('Bonjour ! Quel est ton nom ?')
+__name__ == 'Léan'
+
+if username == 'Léan':
+    print('Enchanté, Léan ! Bienvenue dans le monde de PyCharm')
+else :
+    print("Bonjour " + username)
