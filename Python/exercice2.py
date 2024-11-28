04f595a3b446f485cf7d791d6191c5e790a8371d
Taekyon <106549022+Taekyon@users.noreply.github.com>
2024-11-28 15:43:45 +0100

    Migration PyCharm + Variable
    - Add config files for PyCharm
    - Created variable.py
    - Tried exercice 2

---
+++ Python/exercice2.py
@@ -0,0 +1,18 @@
+print("Inscrivez 7 nombre")
+a = input("Nombre 1: ")
+b = input("2: ")
+c = input("3: ")
+d = input("4: ")
+e = input("5: ")
+f = input("6: ")
+g = input("7: ")
+
+# ne marche pas
+# tester def
+Average = a + b + c + d + e + f + g / 7
+
+# Ajouter
+
+print("Le nombre le plus élevé est : ", Highest)
+print("Le nombre le plus faible est : ", Lowest)
+print("La moyenne est : ", Average)
---
+++ Python/variable.py
@@ -0,0 +1,27 @@
+# Ceci est un commentaire en python.
+# if __name__ == '__main__':
+
+'''
+COMMENTAIRE
+C'est un commentaire en block
+'''
+
+AnneeNaissance = 2001
+AnneCourante = 2024
+
+print(AnneCourante - AnneeNaissance)
+
+Prenom = "Léan"
+Nom = "Asai"
+
+print(Prenom, Nom)
+
+
+x = 23.5
+y = 4.4
+
+print(x + y)
+
+Majeur = True
+
+print("Majeur :", Majeur)
